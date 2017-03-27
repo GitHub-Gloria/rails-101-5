2b878d70f10eb624cd6d6135592f9e0c18f29e84
Xdite <825296448@qq.com>
2017-03-27 19:29:59 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -43,6 +43,32 @@ class GroupsController < ApplicationController
     redirect_to groups_path, alert: "已从列表删除"
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "收藏成功！"
+    else
+      flash[:warning] = "已收藏！"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已取消收藏！"
+    else
+      flash[:warning] = "没有收藏此影评信息，无需反复执行哦！"
+    end
+
+    redirect_to group_path(@group)
+  end  
+
   private
 
   def find_group_and_check_permission
